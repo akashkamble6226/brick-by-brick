9f3b147f4d92c4b408e827ddc3d61de9e42d2b5b
akash <akashkamble6226@gmail.com>
2025-12-31 23:50:36 +0530

    theory question added

---
+++ src/Javascript-interesting-questions/basic-js-questions.js
@@ -0,0 +1,5 @@
+const ans = 0.1 + 0.2
+console.log(ans)
+console.log(ans === 0.3)
+// its false why
+// ans ->   Js uses IEEE 754 floating-point arithmatic, which cannot precisly represent certain decimal numbers in binary
