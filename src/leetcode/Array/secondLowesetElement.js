93aaa95b95fe02ae7ae643a53c470e52ff48fce0
akash <akashkamble6226@gmail.com>
2026-01-21 23:59:39 +0530

    21-1-2026

---
+++ src/leetcode/Array/secondLowesetElement.js
@@ -0,0 +1,30 @@
+//question - given an array find out the second lowest element from array
+
+// thinking
+// if the array is gaurenteed to be distinct , then
+// use sort and return arr[1] elemtn
+
+const arr = [5, 2, 8, 1, 3];
+
+// const sor = arr.sort((a, b) => a - b);
+// console.log(sor[1]);
+//time complexity is -> O(n log n )
+
+// if its not gaurenteed then below is the O(n) solution
+function findSecondLowest(arr) {
+  let lowest = Infinity;
+  let secondLowest = Infinity;
+  for (let i = 0; i < arr.length; i++) {
+    let current = arr[i];
+    if (current < lowest) {
+      secondLowest = lowest;// this has to first
+      lowest = current;
+    } else if (current > lowest && current < secondLowest) {
+      secondLowest = current;
+    }
+  }
+
+  return secondLowest;
+}
+
+console.log(findSecondLowest(arr));
